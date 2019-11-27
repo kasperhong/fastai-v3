59b533af6e1ae02dcd9d0d2848add3dfdc4c39b3
kasperhong <39251375+kasperhong@users.noreply.github.com>
2019-11-27 16:51:56 +0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://drive.google.com/uc?export=download&id=1P1_zp4NvvWvOT5XLmr7myOYunfGbT7kC'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['brain tumor', 'no brain tumor']
 path = Path(__file__).parent
 
 app = Starlette()
